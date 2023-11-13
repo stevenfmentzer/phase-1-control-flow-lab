3868805444cef6e35588d3f5569ba4733d84eb67
Steven Mentzer <stevenfmentzer@gmail.com>
2023-11-12 22:29:56 -0500

    edits in index.js for labwork

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  if (feet <= 400){
+    return ('This one is on me!')
+  } else if (feet > 400 && feet <= 2000) {
+    return ('That will be twenty bucks.')
+  } else if (feet > 2000 && feet <=2500) {
+    return ('I will gladly take your thirty bucks.')
+  } else if (feet > 2500) {
+    return ('No can do.')
+  } else { return}
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  let greeting;
+  city === 'NYC' ? greeting = "Ok, sounds good." : greeting = 'No go.';
+  return greeting;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  let gracious = '';
+  switch(tip){
+    case 'generous':
+      gracious = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      gracious = 'Thank you.';
+      break;
+    case 'thanks for everything':
+      gracious = 'Bye.';
+      break;
+  }
+  return gracious;
 }
